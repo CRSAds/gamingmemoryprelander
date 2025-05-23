a352d20080ab7746eafc319a1cb823f39891a795
CRSAds <yuri@crsadvertising.com>
2025-05-23 18:32:46 +0200

    Delete bedankt.js

--- bedankt.js
+++
@@ -1,6 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const firstname = urlParams.get("firstname");
-const title = document.getElementById("thank-you-name");
-if (firstname && title) {
-  title.textContent = `Bedankt, ${firstname}!`;
-}
